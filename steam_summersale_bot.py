0b4410a56da3f41405029b7bb2b732bd362ca29c
David <davidhodel6@gmail.com>
2018-06-23 15:05:17 +0200

    Deleted unnecesary lines

--- steam_summersale_bot.py
+++ steam_summersale_bot.py
@@ -4,6 +4,8 @@ import cv2
 import time
 import pyautogui
 
+pyautogui.PAUSE = 0.001
+
 last_time = time.time()
 start_time = time.clock()
 kernelOpen=np.ones((5,5))
@@ -12,9 +14,7 @@ lower_range = np.array([80,10,0], np.uint8)
 upper_range = np.array([130,250,250], np.uint8)
 
 while(True):
-    if(round(time.clock() - start_time, 1) / 1000 > 119):
-        quit
-    screen = np.array(ImageGrab.grab(bbox=(637,410,1590,860)))
+    screen = np.array(ImageGrab.grab(bbox=(637,400,1590,860)))
     screen = cv2.cvtColor(screen, cv2.COLOR_BGR2HSV)
 
     print("FPS: {}".format(1/(time.time() - last_time)))
@@ -24,14 +24,11 @@ while(True):
     maskOpen=cv2.morphologyEx(mask,cv2.MORPH_OPEN,kernelOpen)
     maskClose=cv2.morphologyEx(maskOpen,cv2.MORPH_CLOSE,kernelClose)
 
-    maskFinal=maskClose
-    _, conts,h=cv2.findContours(maskFinal.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+    _, conts,h=cv2.findContours(maskClose.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
     cv2.drawContours(screen,conts,-1,(255,0,0),3)
     
     for i in range(len(conts)):
             x,y,w,h=cv2.boundingRect(conts[i])
             pyautogui.click(x + 637, y + 425, clicks=10)
-            # pyautogui.press('1')
-            # pyautogui.press('2')
-            # pyautogui.press('3')
+            pyautogui.press('1')
             break
